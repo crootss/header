edcef3b52fb3b07980518e2cfb9c2b84513a6856
Rolly Maulana Awangga <awangga@gmail.com>
2025-02-24 13:21:23 +0700

    js

--- scrollup/croot.js
+++ scrollup/croot.js
@@ -1,11 +1,11 @@
 import {onClick,container} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.3/element.js";
 
 
-export function afterHeaderLoad(){
-    console.log("header loader");
-    onClick('burger-menu',openMobileMenu);
-    onClick('close-btn',closeMobileMenu);
-}
+
+console.log("header loader");
+onClick('burger-menu',openMobileMenu);
+onClick('close-btn',closeMobileMenu);
+
 
 function openMobileMenu(element){
     container('mobileMenu').style.display = "block";
